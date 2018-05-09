668dba7281f2eafa8f32bb384775a80f30e3730e
Yann Gueguen <yann.gueguen8@gmail.com>
2018-05-09 09:00:35 -0500

    Working on resetting page

--- main.js
+++ main.js
@@ -4,7 +4,23 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-var wordToFind = nouns[getRandomInt(nouns.length)];
+var wordToFind;
+
+document.addEventListener("keypress", function(event) {
+    var h = document.createElement("H1");
+    if (event.key === " "){
+        wordToFind = nouns[getRandomInt(nouns.length)];
+        HTMLFormElement.reset()
+        //var t = document.createTextNode(" ");
+        //h.appendChild(t);
+        //document.body.replaceChild(t,h);
+        for (var i=0 ; i< wordToFind.length; i++){
+            t = document.createTextNode("-");
+            h.appendChild(t);
+            document.body.appendChild(h);
+        }
+    }
+})
 
 document.addEventListener("keypress", function(event) {
     if (wordToFind.includes(event.key) === true){
@@ -14,5 +30,4 @@ document.addEventListener("keypress", function(event) {
             }
         }
     }
-    
 })
