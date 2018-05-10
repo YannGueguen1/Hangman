244a399ec51b4893deb142605948122f933cb6b6
Yann Gueguen <yann.gueguen8@gmail.com>
2018-05-10 08:17:27 -0500

    replaceChild OK

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var nouns = ["home", "work", "apple", "yes", "ciao"];
+var nouns = ["home", "work", "apple", "yes", "ciao", "champion", "france"];
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -10,14 +10,14 @@ document.addEventListener("keypress", function(event) {
     var h = document.createElement("H1");
     if (event.key === " "){
         wordToFind = nouns[getRandomInt(nouns.length)];
-        HTMLFormElement.reset()
-        //var t = document.createTextNode(" ");
+        //document.getElementById("dash").innerHTML = " ";
+        var t;
         //h.appendChild(t);
-        //document.body.replaceChild(t,h);
+        //document.body.removeChild(m);
         for (var i=0 ; i< wordToFind.length; i++){
             t = document.createTextNode("-");
             h.appendChild(t);
-            document.body.appendChild(h);
+            p1 = document.body.appendChild(h);
         }
     }
 })
@@ -26,8 +26,11 @@ document.addEventListener("keypress", function(event) {
     if (wordToFind.includes(event.key) === true){
         for (var i=0 ; i< wordToFind.length; i++){
             if (wordToFind[i] === event.key){
+                var ev = document.createTextNode(event.key);
                 document.getElementById("dash").innerHTML = event.key;
+                p1.replaceChild(ev, p1.childNodes[i]);
             }
         }
     }
 })
+
