a842f39797b4396fadb480f14b9570e270f13f78
Yann Gueguen <yann.gueguen8@gmail.com>
2018-05-14 21:24:31 -0500

    Advanced

--- main.js
+++ main.js
@@ -5,29 +5,35 @@ function getRandomInt(max) {
 }
 
 var wordToFind;
+var error=document.getElementById("counter");
+var errorCounter;
 
 document.addEventListener("keypress", function(event) {
-    var h = document.createElement("H1");
+    var h;
     var t;
     if (event.key === " "){
+        h = document.createElement("div")
         wordToFind = nouns[getRandomInt(nouns.length)];
+        errorCounter = 0;
+        error.innerHTML = errorCounter;
         for (var i=0 ; i< wordToFind.length; i++){
             t = document.createTextNode("-");
             h.appendChild(t);
             p1 = document.body.appendChild(h);
         }
     }
-})
-
-document.addEventListener("keypress", function(event) {
-    if (wordToFind.includes(event.key) === true){
+    else if (wordToFind.includes(event.key) === true){
         for (var i=0 ; i< wordToFind.length; i++){
             if (wordToFind[i] === event.key){
                 var ev = document.createTextNode(event.key);
-                document.getElementById("dash").innerHTML = event.key;
                 p1.replaceChild(ev, p1.childNodes[i]);
             }
         }
     }
+    else{
+        errorCounter = errorCounter + 1;
+        error.innerHTML = errorCounter;
+    }
 })
+
 
