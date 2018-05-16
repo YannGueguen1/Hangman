5fa0d641ea6564afc95ee6c016c56107d28ff34a
Yann Gueguen <yann.gueguen8@gmail.com>
2018-05-15 23:38:50 -0500

    Try new method for dashes

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ function getRandomInt(max) {
 var wordToFind;
 var error=document.getElementById("counter");
 var errorCounter;
-
+var dashString = document.createTextNode("-");
+var isComplete = false;
+/*
 document.addEventListener("keypress", function(event) {
     var h;
     var t;
@@ -28,21 +30,23 @@ document.addEventListener("keypress", function(event) {
             if (wordToFind[i] === event.key){
                 var ev = document.createTextNode(event.key);
                 p1.replaceChild(ev, p1.childNodes[i]);
-
-                isComplete = false;
-                var j;
-                for (j = 0 ; j< wordToFind.length; j++) {
-                    if (p1.childNodes[j] !== "-") {
+                
+                for (var j = 0 ; j< wordToFind.length; j++) {
+                    
+                    //console.log(p1.childNodes[j] !== dashString);
+                    if (p1.childNodes[j] !== dashString) {
                     }
                     else {
-                        isComplete = false;
+                        console.log(isComplete);
+                        console.log(1);
+                        isComplete = true;
                         break;
                     }
                 }
                 if (isComplete) {
                     alert("Word is complete");                
                 }
-                console.log(isComplete);
+                
             }
             
         }
@@ -52,5 +56,47 @@ document.addEventListener("keypress", function(event) {
         error.innerHTML = errorCounter;
     }
 })
-
-
+*/
+var h;
+var t;
+var unknownWord ="";
+document.addEventListener("keypress", function(event) {
+    
+    if (event.key === " "){
+        wordToFind = nouns[getRandomInt(nouns.length)];
+        errorCounter = 0;
+        error.innerHTML = errorCounter;        
+        for (var i=0 ; i< wordToFind.length; i++){
+            unknownWord += "-";
+        }
+        document.getElementById("dash").innerHTML = unknownWord;
+        console.log(unknownWord);
+    }
+    else if (wordToFind.includes(event.key) === true){
+        for (var i=0 ; i< wordToFind.length; i++){
+            
+            if (wordToFind[i] === event.key){
+                unknownWord[i] = unknownWord[i].replace("-", event.key);
+                
+                document.getElementById("dash").innerHTML = unknownWord;
+                for (var j = 0 ; j< wordToFind.length; j++) {
+                    if (unknownWord[j] !== "-") {
+                    }
+                    else {
+                        isComplete = true;
+                        break;
+                    }
+                }
+                if (isComplete) {
+                    alert("Word is complete");                
+                }
+                
+            }
+            
+        }
+    }
+    else{
+        errorCounter = errorCounter + 1;
+        error.innerHTML = errorCounter;
+    }
+})
