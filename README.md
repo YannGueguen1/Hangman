b828fcca7e71df4db9602caf81f059e32fc6e169
YannGueguen1 <yanngueguen2015@u.northwestern.edu>
2021-09-23 05:17:37 -0500

    add section for tries

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Hangman
+
+# How to Play
+Press SPACE to start or to reinitialize a game, and try to guess the word with as little mistakes as possible
--- main.js
+++ main.js
@@ -1,57 +1,76 @@
-var nouns = ["mysterious","official","adult","home", "work", "apple", "yes", "ciao", "champion", "france"];
+var nouns = [
+  "mysterious",
+  "official",
+  "adult",
+  "home",
+  "work",
+  "apple",
+  "yes",
+  "ciao",
+  "champion",
+  "france",
+  "Moscow",
+];
 
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 var wordToFind;
-var error=document.getElementById("counter");
+var error = document.getElementById("counter");
 var errorCounter;
 var dashString = document.createTextNode("-");
 var isComplete;
 var h;
 var t;
 var unknownWord;
-document.addEventListener("keypress", function(event) {
-    
-    if (event.key === " "){
-        wordToFind = nouns[getRandomInt(nouns.length)];
-        unknownWord ="";
-        isComplete = false;
-        errorCounter = 0;
-        error.innerHTML = errorCounter;        
-        for (var i=0 ; i< wordToFind.length; i++){
-            unknownWord += "-";
-        }
+var tries = document.getElementById("tries");
+let allTries=[];
+
+document.addEventListener("keypress", function (event) {
+  if (event.key === " ") {
+    wordToFind = nouns[getRandomInt(nouns.length)];
+    unknownWord = "";
+    isComplete = false;
+    errorCounter = 0;
+    error.innerHTML = errorCounter;
+    for (var i = 0; i < wordToFind.length; i++) {
+      unknownWord += "-";
+    }
+    document.getElementById("dash").innerHTML = unknownWord;
+  } else if (wordToFind.includes(event.key) === true) {
+    for (var i = 0; i < wordToFind.length; i++) {
+      if (wordToFind[i] === event.key) {
+        splitWord = unknownWord.split("");
+        splitWord[i] = event.key;
+        unknownWord = splitWord.join("");
         document.getElementById("dash").innerHTML = unknownWord;
-    }
-    else if (wordToFind.includes(event.key) === true){
-        for (var i=0 ; i< wordToFind.length; i++){
-            if (wordToFind[i] === event.key){
-                splitWord = unknownWord.split("");
-                splitWord[i] = event.key;
-                unknownWord = splitWord.join('');
-                document.getElementById("dash").innerHTML = unknownWord;
-                for (var j = 0 ; j< wordToFind.length; j++) {
-                    if (unknownWord[j] === "-") {
-                        isComplete = false;
-                        break;
-                    }
-                    else {
-                        isComplete = true;
-                    }
-                }
-                if (isComplete) {
-                    alert("Word is complete");
-                    finalErrorCount =  errorCounter;              
-                }
-                
-            }
-            
+        for (var j = 0; j < wordToFind.length; j++) {
+          if (unknownWord[j] === "-") {
+            isComplete = false;
+            break;
+          } else {
+            isComplete = true;
+          }
         }
+        if (isComplete) {
+          alert("Word is complete");
+          finalErrorCount = errorCounter;
+        }
+      }
     }
-    else{
-        errorCounter = errorCounter + 1;
-        error.innerHTML = errorCounter;
-    }
-})
+
+  } else {
+    errorCounter = errorCounter + 1;
+    error.innerHTML = errorCounter;
+  }
+
+  if (allTries.includes(event.key)){
+
+  } else {
+    allTries.push(event.key)
+    let newTry = document.createElement("span");
+    newTry.innerHTML = event.key.toUpperCase() + " ";
+    tries.appendChild(newTry);
+  };
+});
